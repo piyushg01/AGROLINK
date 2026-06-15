4cac1609f9db772502d0b2eee83dfebbeb7777d7
piyushg01 <goswamipiyush100@gmail.com>
2026-06-15 19:45:48 +0530

    Fix AI microservice Render port

--- ai_microservice/app.py
+++ ai_microservice/app.py
@@ -1069,4 +1069,5 @@ def page_not_found(e):
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
